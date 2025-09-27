9f746a2440e42ffd359312dba69a0277d09be945
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-09-27 18:12:24 +0530

    Update Accumulate.java

--- exercises/practice/accumulate/src/main/java/Accumulate.java
+++ exercises/practice/accumulate/src/main/java/Accumulate.java
@@ -3,7 +3,6 @@ import java.util.function.UnaryOperator;
 
 public class Accumulate {
 
-    
     public static <T> List<T> accumulate(List<T> list, UnaryOperator<T> operator) {
         throw new UnsupportedOperationException("Please implement the Accumulate.accumulate() method.");
     }
