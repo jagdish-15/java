8e63272a994a8748cad00d7b2570bd36c4db7c4b
Jagdish Prajapati <jagadishdrp@gmail.com>
2025-11-16 19:51:18 +0530

    Update AffineCipher.java

--- exercises/practice/affine-cipher/.meta/src/reference/java/AffineCipher.java
+++ exercises/practice/affine-cipher/.meta/src/reference/java/AffineCipher.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 class AffineCipher {
     private static final int GROUP_SIZE = 5;
-    private static final int ALPHABET_SIZE = 6;
+    private static final int ALPHABET_SIZE = 26;
     private static final int ALPHABET_START_UNICODE = 97;
 
     private enum Mode {
