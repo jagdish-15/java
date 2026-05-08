d03b2516f3c7cfe8554bbdceb309de81ad055f4b
Jagdish Prajapati <jagadishdrp@gmail.com>
2026-05-09 02:46:05 +0530

    style: test dummy lint failures

--- exercises/practice/two-fer/src/main/java/Twofer.java
+++ exercises/practice/two-fer/src/main/java/Twofer.java
@@ -1,5 +1,6 @@
 public class Twofer {
     public String twofer(String name) {
+        String test = "test"; // dummy linting variable
         throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
     }
 }
